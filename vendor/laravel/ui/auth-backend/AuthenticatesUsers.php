560e18b8445ab466b998e95e69746b3ef4d50e12
syahidcyber05 <syahid0510@gmail.com>
2022-04-07 23:16:09 +0800

    noote module

---
+++ vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
@@ -0,0 +1,290 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use App\Helpers\Lainnya;
+use App\Helpers\Response;
+use curl;
+use App\Models\User;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+trait AuthenticatesUsers
+{
+    use RedirectsUsers, ThrottlesLogins;
+
+    /**
+     * Show the application's login form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showLoginForm()
+    {
+        Lainnya::setSessionToken();
+        return view('home1');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        Lainnya::setSessionToken();
+        $this->validateLogin($request);
+
+        // get API SIA
+        $curl = curl_init();
+        $url ='';
+        $postAr = [
+            'username' => $request->username,
+            'password' => $request->password
+        ];
+        // $url= Response::DomainSIA."/cms-login?h=".Response::HeaderSIA."&app=".Response::AppSIA;
+        $url= session('DomainSIA')."/cms-login?h=".session('HeaderSIA')."&app=".session('AppSIA');
+
+        curl_setopt_array($curl, 
+        array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $postAr,
+            CURLOPT_HTTPHEADER => array(
+                'Cookie: ci_session=f7e9286e42f285d6ecb4028cd78bf2499af77a7f'
+            )
+        ));
+
+        $response = curl_exec($curl);
+        $data = json_decode($response, TRUE);
+        curl_close($curl);
+        if ($data['status'] == 1){
+            if ($data['usergroup'] == 'dosen'){
+                $roleId = 3;
+                $email = $request->username.'@unm.ac.id';
+                
+                $userCms = User::where('username', $request->username)->count();
+                if ($userCms == 0)
+                    $u = $this->CreateUser($request->username,$request->password,$data['nama'],$email,$roleId);
+                else
+                    $u = $this->UpdateUser($request->username, $request->password);
+            }elseif ($data['usergroup'] == 'mahasiswa'){
+                $roleId = 4;
+                $email = $request->username.'@student.unm.ac.id';
+
+                $userCms = User::where('username', $request->username)->count();
+                if ($userCms == 0)
+                    $u = $this->CreateUser($request->username,$request->password,$data['nama'],$email,$roleId);
+                else
+                    $u = $this->UpdateUser($request->username, $request->password);
+            }
+            
+        }
+            // var_dump( $data);
+        // } catch (\Throwable $th) {
+        //     $result = array(
+        //         'status'    => 1,
+        //         'message'    => $th->getMessage(),
+        //     );
+        //     var_dump( $result);
+        // }
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                Lainnya::setSessionToken();
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+    function CreateUser($username, $password, $nama,$email,$roleId)
+    {
+        $user = User::create([
+            'username' => $username,
+            'name' => $nama,
+            'email' => $email,
+            'role_id' => $roleId,
+            'password' => Hash::make($password),
+        ]);
+        return $user;
+    }
+    function UpdateUser($username, $password)
+    {
+        $data = array(
+            'password' =>  Hash::make($password),
+        );
+        $user = User::where('username', $username)
+        ->update($data);
+        return $user;
+    }
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+        ]);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            $this->credentials($request), $request->filled('remember')
+        );
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only($this->username(), 'password');
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+        if ($response = $this->authenticated($request, $this->guard()->user())) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+                    ? new JsonResponse([], 204)
+                    : redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        //
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'email';
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
+    }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
